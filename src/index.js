55153259d905edbc7303a70b74e4890d0ddb647a
Praveen Poonia <Praveen.Poonia@akqa.com>
2019-05-27 17:40:27 +0530

    Added webpack, babel, eslint

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const helloWorld = () => {
+    console.log("Hello World!");	
+    console.log("Arrow functions are working");
+    return "Done";
+};
+helloWorld();
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// webpack.config.js
+module.exports = {
+    entry: __dirname + '/src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        }]
+    }
+};
+	
